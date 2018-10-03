c2ef4d7c7b8bdb0b5999b0ee8b9c949195137dbb
Douglas J Costa <douglas.jesuscosta@gmail.com>
2018-10-03 19:02:04 +0000

    Another commit

--- cats.js
+++ cats.js
@@ -10,7 +10,7 @@ var catSchema = new mongoose.Schema({
 })
 
 var Cat = mongoose.model("Cat", catSchema);
-
+/*
 var george = new Cat({
     name:"Mrs. Norris",
     age: 7,
@@ -25,3 +25,27 @@ george.save(function(err, cat){
         console.log(cat);
     }
 });
+*/
+
+Cat.create({
+    name:"Snow White",
+    age: 15,
+    temperament:"Bland"
+    },
+    function(err, cat){
+        if(err){
+            console.log(err);
+        }else{
+            console.log(cat);
+        }
+    });
+
+Cat.find({}, function(err, cats){
+    if(err){
+        console.log("OH NO, ERROR!");
+    }else{
+        console.log("ALL THE CATS: ");
+        console.log(cats);
+    }
+})
+
